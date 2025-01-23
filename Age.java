c8857430f49e07e51cb547e3e799982d4bb95387
SakthiSarath1945 <164889555+SakthiSarath1945@users.noreply.github.com>
2025-01-23 21:25:42 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Age {
+public static void main(String[] args){
+System.out.println("Enter the age:");
+Scanner sc=new Scanner(System.in);
+int age=sc.nextInt();
+if ((0<=age && 12>=age)==true){
+System.out.println("Child");
+}
+else if (13<=age && 19>=age){
+System.out.println("Teenager");
+}
+else if(20<=age && 64>=age){
+System.out.print("Adult");
+}
+else if (65<=age){
+System.out.print("Senior");
+}
+else{
+System.out.print("Please enter the age above 0");
+}
+}
+}
---
+++ Primeno.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Primeno{
+public static void main(String[] args){
+System.out.print("Enter the number:");
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+int count=0;
+for(int i=2;i<=num;i++){
+if(i%num==0){
+count++;
+}
+}
+if(count==1){
+System.out.print("It is a Prime Number");
+}
+else{
+System.out.print("It is not a Prime Number");
+}
+}
+}
