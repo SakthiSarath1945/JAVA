1da7ff8ba78a2bfac9ce2f17f50f8a44c2c8b843
SakthiSarath1945 <164889555+SakthiSarath1945@users.noreply.github.com>
2025-01-24 22:55:21 +0530

    Add files via upload

---
+++ sumofthreeno.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Sumofthreeno{
+public static void main(String[] args){
+System.out.print("enter the numbers:");
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();
+int sum=a+b+c;
+System.out.print("the sum of three numbers is:"+sum);
+}
+}
